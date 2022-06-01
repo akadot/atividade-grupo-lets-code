065b444efaf33576dd5dd997200f416c0fe6e972
dot_ <muriloholiveira@hotmail.com>
2022-06-01 11:23:40 -0300

    :sparkles: Registers Routes Finished

--- src/controllers/RegistroController.js
+++ src/controllers/RegistroController.js
@@ -1,5 +1,5 @@
 const { readFile, writeFile } = require("../utils/usingFiles")
-
+const api = require("../api/api");
 class RegistroController {
 	index(req, res) {
 		return readFile('./src/data/registros.json', (data) => {
@@ -8,12 +8,77 @@ class RegistroController {
 	}
 
 	store(req, res) {
+		return readFile("./src/data/registros.json", (data) => {
+			req.on('data', async (novoRegistro) => {
+				novoRegistro = JSON.parse(novoRegistro);
 
+				const clientData = await api.get("http://localhost:3000/clientes").then((res) => {
+					return res.data
+				}).catch(err => console.error(err));
+
+				const productData = await api.get("http://localhost:3000/produtos").then((res) => {
+					return res.data
+				}).catch(err => console.error(err));
+
+				const isCliente = await clientData.find(cliente => cliente.id == novoRegistro.clienteID)
+				const isProduct = await productData.find(produto => produto.id == novoRegistro.produtoID)
+				if (isCliente && isProduct) {
+					const lastId = data.lastID;
+					novoRegistro.id = lastId + 1;
+					data.lastID++;
+					data.registros.push(novoRegistro);
+					writeFile("./src/data/registros.json", JSON.stringify(data));
+					res.end("POST Successful!");
+				} else {
+					res.end(`ERRO: Cliente ou Produto não encontrado.\n Cliente: ${isCliente} \n Produto: ${isProduct}`)
+				}
+			})
+		})
 	}
 
-	update(req, res) { }
+	update(req, res) {
+		return readFile("./src/data/registros.json", (data) => {
+			const registroID = req.url.split("s/").pop();
+			const registroExist = data.registros.find(registro => registro.id == registroID)
+			if (registroExist) {
+				req.on('data', (atualizaRegistro) => {
+					atualizaRegistro = JSON.parse(atualizaRegistro);
+					const registroIndex = data.registros.indexOf(registroExist)
+					data.registros[registroIndex] = {
+						id: Number(registroID),
+						tipo: atualizaRegistro.tipo ? atualizaRegistro.tipo : data.registros[registroIndex]["tipo"],
+						clienteID: atualizaRegistro.clienteID ? atualizaRegistro.clienteID : data.registros[registroIndex]["clienteID"],
+						produtoID: atualizaRegistro.produtoID ? atualizaRegistro.produtoID : data.registros[registroIndex]["produtoID"],
+						descricao: atualizaRegistro.descricao ? atualizaRegistro.descricao : data.registros[registroIndex]["descricao"],
+						data: atualizaRegistro.data ? atualizaRegistro.data : data.registros[registroIndex]["data"],
+					};
 
-	delete(req, res) { }
+					writeFile("./src/data/registros.json", JSON.stringify(data));
+					res.end("PUT Successful!");
+				});
+			} else {
+				res.end("Registro não encontrado.")
+			}
+
+		});
+	}
+
+	delete(req, res) {
+		return readFile("./src/data/registros.json", (data) => {
+			const registroID = req.url.split("s/").pop();
+			const registroExist = data.registros.find(registro => registro.id == registroID)
+			if (registroExist) {
+				const registroIndex = data.registros.indexOf(registroExist)
+
+				data.registros.splice(registroIndex, 1);
+				writeFile("./src/data/registros.json", JSON.stringify(data));
+				return res.end("DELETE Successful!");
+			} else {
+				res.end("Registro não encontrado.")
+			}
+
+		});
+	}
 }
 
 module.exports = RegistroController;
--- src/teste.js
+++ src/teste.js
@@ -1,14 +1,16 @@
 const api = require("./api/api");
 
-// api.get("http://localhost:3000/clientes", { nome: "FAUSTO", email: "f@gmail.com", telefone: "123", nomeAnimal: "Celso", tipoAnimal: "Arara Azul" }).then(res => console.log(res.data)).catch(err => console.error(err.data))
+// api.get("http://localhost:3000/registros").then(res => console.log(res.data)).catch(err => console.error(err.data))
 
-// api.post("http://localhost:3000/produtos", {
-// 	nome: "Shampoo",
-// 	valor: 19.9,
-// 	qtde: 25
+// api.post("http://localhost:3000/registros", {
+// 	tipo: "Compra",
+// 	clienteID: 1,
+// 	produtoID: 8,
+// 	descricao: "",
+// 	data: "01-06-2022"
 // }).then(res => console.log(res.data)).catch(err => console.error(err.data))
 
-// api.put("http://localhost:3000/produtos/1", { nome: "Ração de Gato", valor: 39.90 }).then(res => console.log(res.data)).catch(err => console.error(err))
+// api.put("http://localhost:3000/registros/1", { tipo: "Serviço", clienteID: 1, produtoID: 3, descricao: "Banho e Tosa para o cliente 1", data: "20-06-2022" }).then(res => console.log(res.data)).catch(err => console.error(err))
 
-api.delete("http://localhost:3000/produtos/1").then(res => console.log(res.data)).catch(err => console.error(err))
+api.delete("http://localhost:3000/registros/2").then(res => console.log(res.data)).catch(err => console.error(err))
 
